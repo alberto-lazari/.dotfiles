ce5c660f1236ad8397dc1bfb641da20bb0c99707
Alberto Lazari <alberto.lazari@gmail.com>
2023-01-04 17:48:25 +0100

    Multiplatform installation

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/sh -eu
+
+script_dir="$(dirname "$(readlink -f $0)")"
+
+print_help () {
+    echo 'usage: install.sh SHELL' >& 2
+    echo 'SHELL = bash | zsh'
+}
+
+case $(uname) in
+    Darwin) os=macos;;
+    Linux)  os=linux;;
+    *)      echo OS not supported >& 2; exit 1;;
+esac
+
+# Check if $1 is provided
+[ ${1:-unset} = unset ] && { print help; exit 1; }
+
+shell=$1
+
+([ $os = linux ] && [ $shell = zsh ]) && { echo zsh is not yet supported on Linux >& 2; exit 1; }
+
+(grep -E 'bash|zsh' <<< $shell > /dev/null) || { print_help; exit 1; }
+
+for file in $script_dir/default/.[^.]* $script_dir/default/$os/$shell/.[^.]*
+do
+    cp "$file" "$script_dir"
+    
+    actual_file=$(echo $script_dir/$(basename "$file"))
+    home_file="$HOME/$(basename "$file")"
+
+    ([ -f $home_file ] || [ -L $home_file ]) && rm $home_file
+    case $os in
+        macos)  ln -s $actual_file $home_file;;
+        linux)  ln -s $actual_file -t $home_file;;
+    esac
+done
