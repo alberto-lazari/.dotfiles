c0e757afff24c0ef89bd5e4958220513197cb5d5
Alberto Lazari <alberto.lazari@gmail.com>
2023-01-10 22:52:44 +0100

    Zsh setup corrected

--- setup/zsh/setup.sh
+++ setup/zsh/setup.sh
@@ -1,10 +1,17 @@
 #!/bin/bash -eu
 
+# Default zsh variables values
+ZSH=${ZSH:-~/.oh-my-zsh}
+ZSH_CUSTOM=${ZSH_CUSTOM:-$ZSH/custom}
+
+# Load eventual custom zsh variables set in zshrc
+[[ ! -L ~/.zshrc || -e ~/.zshrc ]] || . ~/.zshrc
+
 # Install Oh My Zsh
-[[ -d ${ZSH:-~/.oh-my-zsh} ]] || sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+[[ -d $ZSH ]] || sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install Powerlevel10k theme
-[[ -d $ZSH_CUSTOM/themes/powerlevel10k ]] || git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+[[ -d $ZSH_CUSTOM/themes/powerlevel10k ]] || git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
 # Install custom plugins
 for repo in $(cat plugins.zsh)
