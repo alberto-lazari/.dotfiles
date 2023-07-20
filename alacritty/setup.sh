1db1713f344e96510673e2426e3e67fdd152c88f
Alberto Lazari <alberto.lazari@gmail.com>
2023-07-20 09:51:21 +0200

    Support os-specific configurations

--- alacritty/setup.sh
+++ alacritty/setup.sh
@@ -7,5 +7,12 @@ DIR=~/.config/alacritty
 
 link_files_in . -t $DIR
 
-# Use macOS-adapted icon, since the official one sucks
-[[ $(uname) != Darwin ]] || ./update-icon.sh
+# Load OS-specific configurations
+if [[ $(uname) = Darwin ]]; then
+    link_file os-specific/macos.yml -t $DIR os-specific.yml
+
+    # Use macOS-adapted icon, since the official one sucks
+    ./update-icon.sh
+else
+    link_file os-specific/default.yml -t $DIR os-specific.yml
+fi
