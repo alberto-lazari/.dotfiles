6bfbcbad2e616b5dc5940d3e9ccf23db6738a057
Alberto Lazari <alberto.lazari@gmail.com>
2023-12-31 17:54:36 +0100

    Move to toml

--- alacritty/setup.sh
+++ alacritty/setup.sh
@@ -1,6 +1,6 @@
-#!/bin/bash -e
-
-cd $(dirname $BASH_SOURCE)
+#!/bin/bash
+set -e
+cd "$(dirname "$BASH_SOURCE")"
 
 DIR=~/.config/alacritty
 . ../lib/setup-base.sh
@@ -9,10 +9,10 @@ link_files_in . -t $DIR
 
 # Load OS-specific configurations
 if [[ $(uname) = Darwin ]]; then
-    link_file os-specific/macos.yml -t $DIR os-specific.yml
+    link_file os-specific/macos.toml -t $DIR os-specific.toml
 
     # Use macOS-adapted icon, since the official one sucks
     ./update-icon.sh
 else
-    link_file os-specific/default.yml -t $DIR os-specific.yml
+    link_file os-specific/default.toml -t $DIR os-specific.yml
 fi
