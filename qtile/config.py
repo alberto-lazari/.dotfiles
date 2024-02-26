8c9c059c90c32a5cbf224b4c26de75dfcca5813d
Alberto Lazari <alberto.lazari@gmail.com>
2024-02-26 17:19:03 +0100

    Floating windows have borders

--- qtile/config.py
+++ qtile/config.py
@@ -180,6 +180,9 @@ bring_front_click = False
 floats_kept_above = True
 cursor_warp = False
 floating_layout = layout.Floating(
+    border_focus="#679deb",
+    border_normal="#4c566a",
+    border_width=3,
     float_rules=[
         # Run the utility of `xprop` to see the wm class and name of an X client.
         *layout.Floating.default_float_rules,
