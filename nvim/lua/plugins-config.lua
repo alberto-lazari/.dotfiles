cfa62a6a0b4a9574b28cedafa48fd25fa63118c0
Alberto Lazari <alberto.lazari@gmail.com>
2025-04-15 10:36:38 +0200

    Onedark nvim colorscheme

--- nvim/lua/plugins-config.lua
+++ nvim/lua/plugins-config.lua
@@ -1,9 +1,33 @@
 -- Load Neovim-specific plugins
 vim.fn.LoadPlugins(vim.g.config_dir .. '/plugins.nvim')
 
-require'nvim-treesitter.configs'.setup {
+-- Use Neovim-specific colorscheme
+require 'onedark'.setup {
+  transparent = true,
+  ending_tildes = true,
+
+  -- Don't show comments in italics
+  code_style = {
+    comments = 'none',
+  },
+
+  -- Custom tokens highlights
+  highlights = {
+    ['@comment.todo'] = { fg = '$purple' },
+    ['@string.escape'] = { fg = '$cyan' },
+    ['@type.builtin'] = { fg = '$yellow' },
+    ['@variable.member'] = { fg = '$fg' },
+    ['@field'] = { fg = '$red' },
+    ['@function.builtin'] = { fg = '$blue' },
+    ['@property'] = { fg = '$red' },
+    ['@operator'] = { fg = '$purple' },
+  }
+}
+require 'onedark'.load()
+
+require 'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-  ensure_installed = { all },
+  ensure_installed = { 'comment' },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
